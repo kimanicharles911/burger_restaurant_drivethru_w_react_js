8c852a8e54791c7f923fd8faff7405c6a994e91b
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-26 16:43:59 +0300

    the children prop

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,8 +1,9 @@
-const Person = ({name, age}) => {
+const Person = ({name, age, children}) => {
   return(
-    <>
+    <div>
       <p>I'm {name} and I am {age} years old.</p>
-    </>
+      <p>{children}</p>
+    </div>
   );
 }
 
