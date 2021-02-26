512290e52227a79dccce54a4e92f1ae306df9204
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-26 18:06:30 +0300

    state for beginners

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import './App.css';
 import Person from "./Person/Person"
 
 const App = () => {
+  /* const [location, updateLocation] = useState("Seattle, WA"); */
+  const [personsState, updatePerson] = useState({
+    persons: [
+      { name : "Samson", age: 30 },
+      { name : "Mugo", age : 23},
+      {name : "Marren", age : 25}
+    ]
+  });
+  
   return (
     <div className="App">
       <h1>Hi, I'm a react App</h1>
       <p>This is really working!</p>
-      <Person name="Samson" age="30" />
-      <Person name="Mugo" age="23" >My Hobbies: Racing</Person>
-      <Person name="Marren" age="25" />
+      <button>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
   /* return React.createElement("div",  {className: "App"}, React.createElement("h1", null, "Hi I'm a React App!!!")); */ 
