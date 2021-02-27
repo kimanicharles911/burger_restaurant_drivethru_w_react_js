1ee255a141278ebc6d6bfaaeebc9a3c209f79363
kimanicharles911 <kimanicharles911@gmail.com>
2021-02-27 12:17:46 +0300

    handling events with methods

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ const App = () => {
       {name : "Marren", age : 25}
     ]
   });
+
+  const switchNameHandler = () => {
+    console.log("Collide Air Strike");
+  }
   
   return (
     <div className="App">
       <h1>Hi, I'm a react App</h1>
       <p>This is really working!</p>
-      <button>Switch Name</button>
+      <button onClick={switchNameHandler}>Switch Name</button>
       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Racing</Person>
       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
